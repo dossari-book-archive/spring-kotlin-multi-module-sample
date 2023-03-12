ca9c5c4b04659b117b54f1a95a21218a12b7e132
kazuhisa.honda <kazuhisa.honda@f.flect.co.jp>
2023-03-12 18:42:15 +0900

    初回プロジェクト作成

---
+++ src/main/kotlin/com/example/spring_kotlin_multi_module_sample/SpringKotlinMultiModuleSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_kotlin_multi_module_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinMultiModuleSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinMultiModuleSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/spring_kotlin_multi_module_sample/SpringKotlinMultiModuleSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring_kotlin_multi_module_sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinMultiModuleSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
